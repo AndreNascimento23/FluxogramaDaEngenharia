b613ee01999067d8dc59f9fcbfe55f60e0597b3a
André Nascimento <andre4.20nascimento@outlook.com>
2023-04-19 12:43:30 -0300

    Exercicio

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Clear();
+
+string movendo, mover;
+
+Console.WriteLine("--- Fluxograma Da Engenharia ---");
+
+Console.Write("O objeto esta se movendo (S/N)? ");
+movendo = Console.ReadLine()!.ToUpper();
+
+if (movendo == "N")
+{
+    Console.Write("Deveria (S/N)? ");
+    mover = Console.ReadLine()!.ToUpper();
+    
+    if (mover == "S")
+    {
+        Console.Write("Use WD-40.");
+        Environment.Exit(-1);
+    }
+}
+if (movendo == "S")
+{
+    Console.Write("Deveria (S/N)? ");
+    mover = Console.ReadLine()!.ToUpper();
+    
+    if (mover == "N")
+    {
+        Console.Write("Use Silver Tape.");
+        Environment.Exit(-1);
+    }
+}
+
+ Console.WriteLine("Esta tudo certo");
